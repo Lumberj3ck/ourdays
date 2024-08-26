284074515d0f72f24403c8fc49cb55fcb10e83ce
unknown <alanuldashev@gmail.com>
2024-08-26 20:51:20 +0200

    removed equvivalent version

--- server.py
+++ server.py
@@ -15,17 +15,6 @@ async def client_tiker():
                 print("Plus one")
                 pass
 
-async def recv_msg(websocket):
-    while True:
-        try:
-            message = await websocket.recv()
-            print(message)
-        except websockets.exceptions.ConnectionClosedOK:
-            break
-
-async def end_server(websocket):
-    await websocket.wait_closed()
-
 async def conection_handler(websocket):
     try:
         clients.add(websocket)
@@ -42,25 +31,10 @@ async def conection_handler(websocket):
         clients.remove(websocket)
         print(f"Client disconnected. Remaining clients: {len(clients)}")
 
-async def conection_handler_equvivalent(websocket):
-    try:
-        clients.add(websocket)
-        print("Client conected")
-        await websocket.send("Conected to server!")
-
-        receiver = asyncio.create_task(recv_msg(websocket))
-        gracefully_finish = asyncio.create_task(end_server(websocket))
-
-        await receiver, gracefully_finish
-    finally:
-        clients.remove(websocket)
-        print(f"Client disconnected. Remaining clients: {len(clients)}")
-
-
 
 async def main():
     # tiker = asyncio.create_task(client_tiker())
-    async with websockets.serve(conection_handler_equvivalent, "localhost", 8765):
+    async with websockets.serve(conection_handler, "localhost", 8765):
         print("Server started")
         await asyncio.Future()
 
